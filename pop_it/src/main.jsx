f4e06e882364e24501ffd562dfd0c328d2ce04f9
equis01 <71985026+equis01@users.noreply.github.com>
2025-06-09 16:45:20 -0600

    Update main.jsx

--- pop_it/src/main.jsx
+++ pop_it/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from 'https://evazquez.me/personal-webapps/pop_it/src/App'
+import 'https://evazquez.me/personal-webapps/pop_it/src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
